dbdbb452b860ff84ded1761e5b2c3e1ca4669e0e
David Sveningsson <ext@sidvind.com>
2012-10-05 16:59:24 +0200

    adding json filter.

--- visualizer/filters.py
+++ visualizer/filters.py
@@ -1,6 +1,12 @@
+from json import loads
+
 def csv(value):
     for line in value.splitlines():
         yield tuple([float(x.strip('\x00')) for x in line.split(';')])
+
+def json(value):
+    for row in loads(value):
+        yield row
 
 def extract(index, value):
     yield value[index-1]
